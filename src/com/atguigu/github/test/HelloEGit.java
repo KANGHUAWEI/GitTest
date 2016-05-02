b06b5666fb6e6b73b3057a2f39f0bcce3bc6db92
KANGHUAWEI <ynweikanghua@163.com>
2016-05-02 20:27:14 +0800

    thanks

--- src/com/atguigu/github/test/HelloEGit.java
+++ src/com/atguigu/github/test/HelloEGit.java
@@ -5,5 +5,6 @@ public class HelloEGit {
 	public static void main(String[] args) {
 		System.out.println("github hello 01");
 		System.out.println("github hello 02");
+		System.out.println("回复 01");
 	}
 }
